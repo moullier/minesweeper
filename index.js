3985da7f5c90dcf84ebf4f58011b82b2b179ec51
Maria Francis-Moullier <moullier@gmail.com>
2020-09-26 20:33:44 -0700

    added calculation of numbers for each non-bomb square

--- index.js
+++ index.js
@@ -20,16 +20,87 @@ document.addEventListener('DOMContentLoaded', () => {
             square.classList.add(shuffledArray[i]);
             grid.appendChild(square);
             squares.push(square);
+
+            // listener for click
+            square.addEventListener("click", function(e) {
+                click(square);
+            })
         }
 
         // add numbers to board
         for(let i = 0; i < squares.length; i++) {
+            let total = 0;
             const isLeftEdge = i % width === 0;
             const isRightEdge = i % width === width - 1;
+            console.log(i);
 
+            if(squares[i].classList.contains("empty")) {
+                // check square to left for bomb
+                if(i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb")) {
+                    total++;
+                }
+
+                // check square to upper right for bomb
+                if(i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains("bomb")) {
+                    total++;
+                }
+
+                // check square above for bomb
+                if(i > 9 && squares[i - width].classList.contains("bomb")) {
+                    total++;
+                }
+                
+                // check square to upper left for bomb
+                if(i > 10 && !isLeftEdge && squares[i - 1 - width].classList.contains("bomb")) {
+                    total++;
+                }
+
+                // check square to right for bomb
+                if(i < 99 && !isRightEdge && squares[i + 1].classList.contains("bomb")) {
+                    total++;
+                }
+
+                // check square to lower left for bomb
+                if(i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains("bomb")) {
+                    total++;
+                }
+
+                // check square to lower right for bomb
+                if(i < 90 && !isRightEdge && squares[i + 1 + width].classList.contains("bomb")) {
+                    total++;
+                }
+
+                // check square below for bomb
+                if(i < 90 && squares[i + width].classList.contains("bomb")) {
+                    total++;
+                }
+
+                squares[i].setAttribute("data", total);
+                squares[i].innerHTML = total;
+
+                console.log(squares[i]);
+            }
         }
     }
 
     createBoard();
+
+
+    // click on square actions
+    function click(square) {
+        if(square.classList.contains("bomb")) {
+            alert("game over");
+        } else {
+            let total = square.getAttribute("data");
+            if (total != 0) {
+                square.classList.add("checked");
+                square.innerHTML = total;
+                return;
+            }
+        }
+    }
+
+
+
 
 })
